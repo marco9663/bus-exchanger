84826cdb2a3f2907ac940453114db40df4177b36
Marco Yuen <marcoyuen2006@gmail.com>
2023-01-15 22:33:57 +0800

    update to nextjs 13

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -17,7 +17,6 @@ const buttons = [
 
 export const Footer: React.FC = () => {
     const router = useRouter();
-    console.log(router.asPath);
     return (
         <div className="flex justify-evenly items-center bg-blue-200 dark:bg-blue-900 px-4 py-2 h-12">
             {buttons.map((btn) => (
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,12 @@
 import { FC, ReactNode } from "react";
 
-import { Footer } from "..";
+import { Footer } from "../footer";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col justify-between bg-blue-50 dark:bg-gray-900">
             <main className="grow">{children}</main>
-            <Footer></Footer>
+            <Footer />
         </div>
     );
 };
