57c447eb536074ac34294a2f3d7677071371bdb8
Marco Yuen <marcoyuen2006@gmail.com>
2023-01-15 22:51:48 +0800

    add PWA support

--- next.config.js
+++ next.config.js
@@ -3,14 +3,19 @@
 // module.exports = withPlugins([], {});
 
 const nextTranslate = require("next-translate");
+const withPWA = require("next-pwa")({
+    dest: "public",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = nextTranslate({
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: ["lh3.googleusercontent.com", "i.imgur.com"],
-    },
-});
+const nextConfig = withPWA(
+    nextTranslate({
+        reactStrictMode: true,
+        swcMinify: true,
+        images: {
+            domains: ["lh3.googleusercontent.com", "i.imgur.com"],
+        },
+    }),
+);
 
 module.exports = nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,35 @@ class CustomDocument extends Document {
     render(): JSX.Element {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <meta name="application-name" content="BUS Exchanger" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta
+                        name="apple-mobile-web-app-status-bar-style"
+                        content="default"
+                    />
+                    <meta name="apple-mobile-web-app-title" content="PWA App" />
+                    <meta
+                        name="description"
+                        content="Bus exchange information"
+                    />
+                    <meta name="format-detection" content="telephone=no" />
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    <meta
+                        name="msapplication-config"
+                        content="/icons/browserconfig.xml"
+                    />
+                    <meta name="msapplication-TileColor" content="#2B5797" />
+                    <meta name="msapplication-tap-highlight" content="no" />
+                    <meta name="theme-color" content="#000000" />
+
+                    <link
+                        rel="apple-touch-icon"
+                        href="/icons/icon-256x256.png"
+                    ></link>
+
+                    <link rel="manifest" href="/manifest.json" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
