2d363de9760b8117f0511d222e43d8f3df0d469b
Marco Yuen <marcoyuen2006@gmail.com>
2023-01-19 10:34:30 +0800

    working index page infinite scroll

--- db.ts
+++ db.ts
@@ -1,12 +1,6 @@
 import Dexie, { Table } from "dexie";
 import { KMBRouteStop, KMBRouteType } from "@apis";
 
-export interface Friend {
-    id?: number;
-    name: string;
-    age: number;
-}
-
 export interface KMBRouteTable extends KMBRouteType {
     id?: number;
 }
@@ -21,18 +15,24 @@ export interface KMBRouteStopTable extends KMBRouteStop {
     long: string;
 }
 
+export interface Hash {
+    id: string;
+    value: string;
+}
+
 export class MySubClassedDexie extends Dexie {
     // 'friends' is added by dexie when declaring the stores()
     // We just tell the typing system this is the case
     kmbRouteTable!: Table<KMBRouteTable>;
     kmbRouteStopTable!: Table<KMBRouteStopTable>;
-
+    hash!: Table<Hash>;
     constructor() {
         super("myDatabase");
         this.version(1).stores({
             kmbRouteTable:
                 "++id, route, orig_en, orig_tc, dest_en, dest_sc, [route+bound+service_type]",
             // kmbRouteStopTable: "++id, route, stop , [route+bound+service_type]"
+            hash: "id",
         });
     }
 }
